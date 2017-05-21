5b9555f8226e53051c53f8f1adaf29804d2395a8
未来 <aiastia@users.noreply.github.com>
2017-05-21 10:40:15 +0800

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -2,4 +2,4 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-wget -N --no-check-certificate https://raw.githubusercontent.com/aiastia/k-z/master/ss.sh?token=AS3DXmGDnaG_RvqTWdQx6-wYhzRnV2x6ks5ZKjFOwA%3D%3D && bash ss.sh && rm -rf *.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/aiastia/k-z/master/ss.sh && bash ss.sh && rm -rf *.sh
